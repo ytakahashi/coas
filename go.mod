2d5221acd66603f7f101f5b6fc50635264a0055e
ytakahashi <yt01tc@gmail.com>
2021-03-20 05:25:50 +0900

    feat: command (#7)

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/getkin/kin-openapi v0.34.0
 	github.com/ktr0731/go-fuzzyfinder v0.3.2
 	github.com/manifoldco/promptui v0.8.0
+	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.7.0
 )
--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/ytakahashi/coas/internal"
-)
+import "github.com/ytakahashi/coas/cmd"
 
 func main() {
-	file := os.Args[1]
-	swagger, err := openapi3.NewSwaggerLoader().LoadSwaggerFromFile(file)
-	if err != nil {
-		panic(err)
-	}
-
-	apis := internal.ParsePaths(swagger.Paths)
-	selected := internal.SelectAPI(apis)
-	url := internal.BuildURL(selected, new(internal.PromptUI))
-	fmt.Println(url)
+	cmd.Execute()
 }
